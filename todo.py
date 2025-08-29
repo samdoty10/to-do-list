d31ab66549f5b5031a01876a90b75d39e29ee829
samdoty10 <samdoty208@gmail.com>
2025-08-29 09:31:01 -0500

    Added mark complete function

--- todo.py
+++ todo.py
@@ -19,7 +19,9 @@ def delete_task(remove):
 
 
 # Step 5: Mark task complete
-
+def mark_complete(index):
+    if 0<= index < len(tasks):
+        tasks[index] = tasks[index] + " ✅"
 
 # Step 6: Save/load tasks (extra stretch for today)
 
@@ -30,6 +32,7 @@ if __name__ == "__main__":
     add_task("Push code to GitHub")
     view_tasks()
     delete_task(0)
+    mark_complete(0)
     view_tasks()
     #mark_complete(0)
     #view_tasks()
